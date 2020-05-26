5c06cd74b60405c91f82161af24b24c6250acbbd
Adam Bien <abien@adam-bien.com>
2020-05-26 11:42:35 +0200

    refactored to multi-module

---
+++ src/main/resources/archetype-resources/__rootArtifactId__/src/main/java/airhacks/service/JAXRSConfiguration.java
@@ -0,0 +1,15 @@
+package airhacks.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint. Delete this class, if you are not exposing
+ * JAX-RS resources in your application.
+ *
+ * @author airhacks.com
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
---
+++ src/main/resources/archetype-resources/__rootArtifactId__/src/main/java/airhacks/service/ping/boundary/PingResource.java
@@ -0,0 +1,24 @@
+package airhacks.service.ping.boundary;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@Path("ping")
+public class PingResource {
+
+    @Inject
+    @ConfigProperty(name = "message")
+    String message;    
+
+    @GET
+    public String ping() {
+        return this.message + " Jakarta EE 8 with MicroProfile 3+!";
+    }
+
+}
